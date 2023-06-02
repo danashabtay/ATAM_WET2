2052a246a55385d710c30c187e71f7db81cda704
danashabtay <99danashabtay@gmail.com>
2023-06-02 17:26:04 +0300

    Update ili_handler.asm

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -2,88 +2,101 @@
 
 .text
 .align 4, 0x90
-
 my_ili_handler:
-  pushq %rax
-  pushq %rbx
-  pushq %rcx
-  pushq %rdx	
-  pushq %r8
-  pushq %r9
-  pushq %r10
-  pushq %r11
-  pushq %r12
-  pushq %r13
-  pushq %r14
-  pushq %r15
-  pushq %rsi
-  pushq %rbp
-  pushq %rsp
+	pushq %rax
+	pushq %rbx
+	pushq %rcx
+	pushq %rdx	
+	pushq %r8
+	pushq %r9
+	pushq %r10
+	pushq %r11
+	pushq %r12
+	pushq %r13
+	pushq %r14
+	pushq %r15
+	pushq %rsi
+	pushq %rbp
+	pushq %rsp
+	 
+	#%rip value (command address) ro %rbx
 
-  xorq %rdi, %rdi
-  xorq %rax, %rax
-  xorq %rcx,%rcx
-  movq 120(%rsp),%rcx
-  movq (%rcx), %rcx
-  cmpb $0x0f, %cl
-  jne one_byte
+	xorq %rbx, %rbx
+	xorq %rcx, %rcx
+	xorq %rdi,%rdi
 
-  movb %ch, %al
-  movq %rax, %rdi
-  call what_to_do
-  cmpq $0, %rax
-  je default_handler
-  jmp our_handler
+	xorq %rax, %rax
+	movq (%rsp), %rbx 	
+	movq (%rbx), %rbx
 
-one_byte:
+	#compare the first byte in %al of the opcode to 0X0F 
+	movq $1, %rcx
+	cmpb $0x0F, %bl
+	jne ONE_BYTE
 
-  movb %cl, %al
-  movq %rax, %rdi
-  call what_to_do
-  cmpq $0, %rax
-  je default_handler
-  jmp our_handler
+	cmp $0x3A, %bh
+	je ONE_BYTE
+	cmp $0x38, %bh
+	je ONE_BYTE
 
-default_handler:
-  popq %rsp
-  popq %rbp
-  popq %rsi
-  popq %r15
-  popq %r14
-  popq %r13
-  popq %r12
-  popq %r11
-  popq %r10
-  popq %r9
-  popq %r8
-  popq %rdx
-  popq %rcx
-  popq %rbx
-  popq %rax
+	#two byte opcode the last byte is stored in %ah
+	movb %bh, %al
+	movq %rax, %rdi
+	movq $2, %rcx
+	jmp CALL_WHATTODO
 
-  jmp * old_ili_handler
-  jmp finish
+	ONE_BYTE:
+	#the byte is stored in %al
+	movq $1, %rcx
+	movb %bl, %al
+	movq %rax, %rdi
+	  
+	CALL_WHATTODO:
+	call what_to_do
+	cmp $0, %rax
+	jne NOT_ZERO
 
-our_handler:
-  movq %rax, %rdi
-  
-  popq %rsp
-  popq %rbp
-  popq %rsi
-  popq %r15
-  popq %r14
-  popq %r13
-  popq %r12
-  popq %r11
-  popq %r10
-  popq %r9
-  popq %r8
-  popq %rdx
-  popq %rcx
-  popq %rbx
-  popq %rax
-	
-  addq $2, (%rsp) 
-	
-finish:
-  iretq
+	#if return value is zero:
+	old_handler:
+	popq %rsp
+	popq %rbp
+	popq %rsi
+	popq %r15
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %r11
+	popq %r10
+	popq %r9
+	popq %r8
+	popq %rdx
+	popq %rcx
+	popq %rbx
+	popq %rax
+	jmp * old_ili_handler
+	jmp END
+
+	#if return value is not zero:
+	NOT_ZERO:
+	movq %rax, %rdi
+
+	popq %rsp
+	popq %rbp
+	popq %rsi
+	popq %r15
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %r11
+	popq %r10
+	popq %r9
+	popq %r8
+	popq %rdx
+	popq %rcx
+	popq %rbx
+	popq %rax
+
+	addq $2, (%rsp) 
+
+	END:
+	iretq
